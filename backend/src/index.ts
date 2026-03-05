3ceecb5317def5959f2eaab91183a15e03f45431
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-03-05 20:21:51 +0000

    chore(BE): install and setup helmet, cors and rate limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,25 @@
-import express from "express";
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { rateLimiter } from './middleware/rate-limiter.js';
 
 const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-app.listen(3000, () => {
-  console.log("Server started");
+if (!PORT || !CLIENT_URL) {
+  throw new Error('Missing required environment variables');
+}
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: 'GET',
+  }),
+);
+app.use(rateLimiter);
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT} port`);
 });
