a5c8b3c10bb9bd69e469e09e3e1d75ae6e080dd5
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-03-07 13:27:33 +0000

    feat(BE): move server related code from index.ts to server.ts, handle graceful close

---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { rateLimiter } from './middleware/rate-limiter.js';
+import { githubClient } from './lib/github-client.js';
+import { env } from './common/env.js';
+import { errorHandler } from './middleware/error-handler.js';
+import { requestLogger } from './middleware/request-logger.js';
+
+export const app = express();
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: env.CLIENT_URL,
+    methods: 'GET',
+  }),
+);
+app.use(requestLogger);
+app.use(rateLimiter);
+
+app.get('/status', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.get('/users', async (req, res) => {
+  const username = req.query.username;
+
+  if (!username) {
+    return res.status(400).json({ message: 'Username is required' });
+  }
+
+  const response = await githubClient.get(`/users/${username}`);
+  res.status(200).json(response);
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(errorHandler);
