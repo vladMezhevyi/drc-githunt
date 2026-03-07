233d8e37fe0a0c0bdb73b4978a2905a207ae9fd3
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-03-07 14:38:23 +0000

    refactor(BE): use octokit for making github requests instead of custom wrapper

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import { rateLimiter } from './middleware/rate-limiter.js';
-import { githubClient } from './lib/github/github-client.js';
 import { env } from './common/env.js';
 import { errorHandler } from './middleware/error-handler.js';
 import { requestLogger } from './middleware/request-logger.js';
@@ -21,17 +20,6 @@ app.use(rateLimiter);
 
 app.get('/status', (req, res) => {
   res.status(200).json({ status: 'ok' });
-});
-
-app.get('/users', async (req, res) => {
-  const username = req.query.username;
-
-  if (!username) {
-    return res.status(400).json({ message: 'Username is required' });
-  }
-
-  const response = await githubClient.get(`/users/${username}`);
-  res.status(200).json(response);
 });
 
 app.use((req, res) => {
