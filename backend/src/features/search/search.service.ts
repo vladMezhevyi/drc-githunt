a894b628a37755f17fa5780e1d2704f877c39e36
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-03-07 22:37:50 +0000

    feat(BE): handle search service github api errors

--- backend/src/features/search/search.service.ts
+++ backend/src/features/search/search.service.ts
@@ -7,15 +7,20 @@ import type {
 } from './search.schema.js';
 
 class SearchService {
+  private readonly errors: Record<number, string> = {
+    422: 'Invalid search query',
+    503: 'GitHub search service is unavailable, please try again later',
+  };
+
   async searchRepositories(
     queryParams: SearchRepositoriesQueryParams,
   ): Promise<SearchRepositoriesResponse> {
-    const response = await githubRequest('GET /search/repositories', queryParams);
+    const response = await githubRequest('GET /search/repositories', queryParams, this.errors);
     return response.data;
   }
 
   async searchUsers(queryParams: SearchUsersQueryParams): Promise<SearchUsersResponse> {
-    const response = await githubRequest('GET /search/users', queryParams);
+    const response = await githubRequest('GET /search/users', queryParams, this.errors);
     return response.data;
   }
 }
