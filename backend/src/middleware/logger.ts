60f9bdd80f8a969a1e98e92b1c2205ff1c1ed22f
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-03-07 11:27:06 +0000

    feat(BE): setup logger

---
+++ backend/src/middleware/logger.ts
@@ -0,0 +1,18 @@
+import pino from 'pino';
+import { env } from '../common/env.js';
+
+const isDev: boolean = env.NODE_ENV !== 'production';
+
+export const logger = pino({
+  level: isDev ? 'debug' : 'info',
+  transport: isDev
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
+});
